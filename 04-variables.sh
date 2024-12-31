a23e97991e829b3bf46b6275252d838735e65865
Ajay2625 <ajayreddykondeti@gmail.com>
2024-12-31 00:19:42 -0800

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=ajay
+PERSON2=vijay
+
+echo "${PERSON1}:Hi ${PERSON2},how are you doing?"
+echo "${PERSON2}:Iam doing great,Thanks.How are you?"
+echo "${PERSON1}:Iam doing good"
