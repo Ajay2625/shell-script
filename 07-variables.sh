4d22c64c0a419ff2e5792a8e60072bea2e303a53
Ajay2625 <ajayreddykondeti@gmail.com>
2024-12-31 00:35:09 -0800

    variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -4,5 +4,5 @@ NAMES=("Ajay" "Avinash" "Akhil" "Manideep")
 
 echo "First Person Name:${NAMES[0]}"
 echo "Second Person Name:${NAMES[1]}"
-echo "Third Person Name:"${NAMES[2]}"
+echo "Third Person Name:${NAMES[2]}"
 echo "Fourth Person Name:${NAMES[3]}"
