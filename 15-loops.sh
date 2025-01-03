91b4273fd4a92abf3e351bb40d1619fdd52642f8
Ajay2625 <ajayreddykondeti@gmail.com>
2025-01-03 12:49:36 -0800

    variables

--- 15-loops.sh
+++ 15-loops.sh
@@ -9,10 +9,10 @@ N="\e[0m"
 FILE_FOLDER_NAME="/var/log/shell-scripting.logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$FILE_FOLDER_NAME/$LOG_FILE/$TIMESTAMP.log"
+LOG_FILE_NAME="$FILE_FOLDER_NAME/$LOG_FILE-$TIMESTAMP.log
  
 VALIDATE(){
-    if [ $1 ne 0 ]
+    if [ $1 -ne 0 ]
     then 
         echo -e "$2 $R FAILURE $N"
         exit 1
@@ -21,9 +21,8 @@ VALIDATE(){
     fi
 }
 
-echo "Script started executing at :$TIMESTAMP" &>>LOG_FILE_NAME
-
-if [ $USERID ne 0 ]
+echo "Script started executing at :$TIMESTAMP" &>>$LOG_FILE_NAME
+if [ $USERID -ne 0 ]
 then 
     echo "You must have sudo access to execute this script"
     exit 1
@@ -31,7 +30,7 @@ fi
 
 dnf list installed mysql &>>$LOG_FILE_NAME
 
-if [ $? ne 0 ]
+if [ $? -ne 0 ]
 then 
     dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing Mysql"
@@ -41,10 +40,10 @@ fi
 
 dnf list installed git &>>$LOG_FILE_NAME
 
-if [ $? ne 0 ]
+if [ $? -ne 0 ]
 then 
     dnf install git -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing Git"
 else 
-    echo -e 'Git Already.... $Y INSTALLED $N'
+    echo -e "Git Already.... $Y INSTALLED $N"
 fi
